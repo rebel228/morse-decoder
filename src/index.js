b74d4a52ac75213253e5a473e36eb2c6ff6ec60e
rebel228 <dinovik@rambler.ru>
2023-02-25 20:56:47 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let expression = expr;
+    //breaking string in 10-char slices
+    while (expression.length > 0) {
+        let element = expression.slice(0, 10);
+        expression = expression.substring(10);
+        if (element === '**********') {
+            result += ' ';
+        }
+        else {
+            //removing all extra zeroes in front
+        let actualString = '';
+        for (let i = 0; i < 10; i ++) {
+            if (element[i] === '1') {
+                actualString = element.substring(i);
+                break;
+            }
+        }
+        //turning every 2characters into morse code
+        let code = ''; //dots and dashes will go here
+        for (let i = 0; i <actualString.length; i += 2) {
+            if (actualString.slice(i, i + 2) === '10') code += '.';
+            else code += '-';
+        }
+        //comparing code with the letters and adding to the final string
+        result += MORSE_TABLE[code];
+        }
+    }
+
+
+    return result;
 }
 
 module.exports = {
